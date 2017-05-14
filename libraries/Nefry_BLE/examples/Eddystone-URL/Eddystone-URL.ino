7344e0bc66364ecebb4039a97fc56f373c53872b
wami <wamiwami@live.jp>
2017-05-15 00:09:55 +0900

    サンプルプログラム更新

--- libraries/Nefry_BLE/examples/Eddystone-URL/Eddystone-URL.ino
+++ libraries/Nefry_BLE/examples/Eddystone-URL/Eddystone-URL.ino
@@ -1,9 +1,9 @@
 #include "NefryEddystone.h"
 
 void setup() {
-    Eddystone.begin();
-	Eddystone.setUrl("google.com");
-    Serial.println("Advertise URL");
+  NefryEddystone.begin();
+  NefryEddystone.setUrl("google.com");
+  Serial.println("Advertise URL");
 }
 
 void loop() {
